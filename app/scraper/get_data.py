755186fcf9206ae9c390a24f411cf0990fc23a46
Sam Dillard <sam@influxdata.com>
2021-01-14 17:54:10 -0800

    add hashing for pages to monitor existence of page changes

--- app/scraper/get_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from PandasBasketball.errors import TableNonExistent
-from PandasBasketball.stats import player_stats, team_stats, player_gamelog, n_days
-
-
-from models import Player
-
--- app/scraper/getters.py
+++ app/scraper/getters.py
@@ -4,6 +4,7 @@ import string
 from bs4 import BeautifulSoup
 from PandasBasketball import pandasbasketball as pb
 from PandasBasketball.stats import player_stats, team_stats, player_gamelog, n_days
+import hashlib
 
 def get_players_letter_dirs():
     """Returns list of URLs where each URL holds all the players
@@ -53,5 +54,20 @@ def get_full_player_urls():
             if encoding.startswith(letter_dir[-2]):
                 full_player_urls.append(letter_dir+encoding+".html")
     return full_player_urls
+
+def hash_page(url):
+    """
+    Hashes a webpage.  Old hashes will be compared with new 
+    hashes to monitor the existence of page changes.
+    """
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, "lxml")
+    return hashlib.md5(repr(soup).encode("utf-8"))
+
+def compare_hash(hash1, hash2):
+    """Return True if hashes are equal. Used for monitoring changes in webpages"""
+    return hash1 == hash2
+
+
 
 # def get_latest(table)
